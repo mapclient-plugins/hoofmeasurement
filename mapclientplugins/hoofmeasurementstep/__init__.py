997596d8339487c5acd18c1b110ea630e9269157
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-06-23 13:50:45 +1200

    Set location of repository.

--- mapclientplugins/hoofmeasurementstep/__init__.py
+++ mapclientplugins/hoofmeasurementstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Hoof Measurement'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/hoofmeasurement.git'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.hoofmeasurementstep import step
