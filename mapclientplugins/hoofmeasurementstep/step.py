75ad2c8cd1f29e9a3edbc93ea52cc335eaa44e07
TimmyNic <tim.nicolas@outlook.com>
2021-06-11 12:36:58 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/hoofmeasurementstep/step.py
+++ mapclientplugins/hoofmeasurementstep/step.py
@@ -93,7 +93,7 @@ class HoofMeasurementStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
