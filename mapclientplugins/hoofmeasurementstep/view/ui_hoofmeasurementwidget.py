93ca7e21e8505afd29be104ba33cec2543cf729b
TimmyNic <tim.nicolas@outlook.com>
2021-06-11 12:45:43 +1200

    Updated .ui file.

--- mapclientplugins/hoofmeasurementstep/view/ui_hoofmeasurementwidget.py
+++ mapclientplugins/hoofmeasurementstep/view/ui_hoofmeasurementwidget.py
@@ -12,7 +12,7 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-from zinchoofmeasurementwidget import ZincHoofMeasurementWidget
+from mapclientplugins.hoofmeasurementstep.view.zinchoofmeasurementwidget import ZincHoofMeasurementWidget
 
 
 class Ui_HoofMeasurementWidget(object):
